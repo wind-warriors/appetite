c64a0893f1b1ec8dc15ef98122985e00cb698341
Rafael Timbó <timbo.rafa@gmail.com>
2018-02-28 21:02:59 -0500

    Adding android.permission.INTERNET

---
+++ app/src/main/java/com/example/rafael/appetite/YelpService/Yelp.kt
@@ -0,0 +1,9 @@
+package com.example.rafael.appetite.YelpService
+
+/**
+ * Created by rafael on 28/02/18.
+ */
+class YelpService {
+    val yelpSearchUrl: String = "https://api.yelp.com/v3/businesses/search"
+    val yelpBusinessUrl: String = "https://api.yelp.com/v3/businesses/"
+}
---
+++ app/src/test/java/com/example/rafael/appetite/YelpService/YelpTest.kt
@@ -0,0 +1,20 @@
+package com.example.rafael.appetite.YelpService
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+/**
+ * Created by rafael on 28/02/18.
+ */
+class YelpTest {
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+}
