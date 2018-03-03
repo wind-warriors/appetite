32660817bd4566e75974d741a4e496763b15ca20
singh.kamall <preetkamal110@gmail.com>
2018-03-02 22:52:12 -0500

    Added Register Activity Added gson library

---
+++ app/src/main/java/com/windwarriors/appetite/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.windwarriors.appetite;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
