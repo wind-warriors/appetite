f21b4a1d3ce4815b9ca59d2a4a0830b3dfc9c4fc
singh.kamall <preetkamal110@gmail.com>
2018-03-03 00:37:39 -0500

    Registration implemented using SharedPreferences

--- app/src/main/java/com/windwarriors/appetite/MainActivity.kt
+++ app/src/main/java/com/windwarriors/appetite/MainActivity.kt
@@ -2,11 +2,36 @@ package com.windwarriors.appetite
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.menu_register -> {
+                startActivity(Intent(this, RegisterActivity::class.java))
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/windwarriors/appetite/RegisterActivity.java
+++ app/src/main/java/com/windwarriors/appetite/RegisterActivity.java
@@ -1,16 +1,121 @@
 package com.windwarriors.appetite;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.reflect.TypeToken;
+import com.windwarriors.appetite.model.UserModel;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import  com.google.gson.Gson;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 
+    public void btnRegister_OnClick(View view) {
+
+        final EditText etEmail = (EditText)findViewById(R.id.etEmail);
+        EditText etPassword = (EditText)findViewById(R.id.etPassword);
+        EditText etConfirmPassword = (EditText)findViewById(R.id.etConfirmPassword);
+
+        String email = etEmail.getText().toString().toLowerCase().trim();
+        String password = etPassword.getText().toString().trim();
+        String confirmPassword = etEmail.getText().toString().toLowerCase().trim();
+
+        if(etEmail.getText().toString().trim().length() == 0 ||
+                etPassword.getText().toString().trim().length() == 0 ||
+                etConfirmPassword.getText().toString().trim().length() == 0){
+
+            Toast.makeText(this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+            return;
+
+        } else if (etPassword.getText().toString().trim().length() < 6) {
+
+            Toast.makeText(this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
+            return;
+
+        } else if (!etPassword.getText().toString().trim().equals(etConfirmPassword.getText().toString().trim())) {
+
+            Toast.makeText(this, "Password and Confirm Password must match", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        List<UserModel> spUsers = loadUsersFromSharedPreferences(this);
+        
+        if(spUsers == null){
+            // Register User
+            spUsers = new ArrayList<UserModel>();
+
+            UserModel uModel = new UserModel(
+                    etEmail.getText().toString().toLowerCase().trim(),
+                    etPassword.getText().toString().trim());
+
+            spUsers.add(uModel);
+
+        } else {
+
+            for (UserModel _user : spUsers) {
+                if (_user.getEmail().equals(etEmail.getText().toString().trim())) {
+                    Toast.makeText(this, "Email already registered", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+            // Register User
+            UserModel uModel = new UserModel(
+                    etEmail.getText().toString().toLowerCase().trim(),
+                    etPassword.getText().toString().trim());
+            spUsers.add(uModel);
+
+        }
+        saveUsersToSharedPreferece(this, spUsers);
+        Toast.makeText(this, "User registered successfully", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public static void saveUsersToSharedPreferece(Context context, List<UserModel> spUsers) {
+        SharedPreferences mPrefs = context.getSharedPreferences("users", context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(spUsers);
+        prefsEditor.putString("myJson", json);
+        prefsEditor.commit();
+    }
+
+    public static List<UserModel> loadUsersFromSharedPreferences(Context context) {
+        List<UserModel> spUsers = new ArrayList<UserModel>();
+        
+        SharedPreferences mPrefs = context.getSharedPreferences("users", context.MODE_PRIVATE);
+        
+        Gson gson = new Gson();
+        
+        String json = mPrefs.getString("myJson", "");
+        
+        if (json.isEmpty()) {
+            return null;
+        } else {
+            Type type = new TypeToken<List<UserModel>>() {
+                
+            }.getType();
+            spUsers = gson.fromJson(json, type);
+        }
+        return spUsers;
     }
 
 }
